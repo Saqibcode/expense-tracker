32caeff7eab0a3631b966848cc6f952a4a86e5bf
Saqibcode <saqib.u@hotmail.com>
2021-03-10 12:49:31 +0000

    expense tracker first commit

---
+++ src/components/TransactionList.js
@@ -0,0 +1,24 @@
+import React, {useContext} from 'react'
+import { GlobalContext } from "../context/GlobalState";
+
+import Transaction from "./Transactions";
+
+function TransactionList() {
+    const {transactions} = useContext(GlobalContext);
+
+    
+
+    return (
+        <>
+        <h3>Items</h3>
+      <ul className="list">
+          {transactions.map(transaction=>(
+            <Transaction key={transaction.id} transaction={transaction}/>
+          ))}
+        
+      </ul>
+        </>
+    )
+}
+
+export default TransactionList
